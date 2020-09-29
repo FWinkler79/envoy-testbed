9da0917e6c6f4a78a57b7746a3b1eeea013a7dcc
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2020-09-29 20:08:28 +0200

    working

--- client/application/src/main/java/com/equalities/envoy/client/EchoClient.java
+++ client/application/src/main/java/com/equalities/envoy/client/EchoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "Server", url = "http://localhost")
+@FeignClient(name = "Server", url = "http://client-envoy")
 public interface EchoClient {
 
   @GetMapping(path="/echo/{message}" )
---
+++ startLandscape.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -f server/docker-compose.yaml -f client/docker-compose.yaml up -d
